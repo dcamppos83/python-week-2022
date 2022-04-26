03c35a371e13cfb95b0a149cdf1abb29846b18f6
dcamppos83 <danielcamppos@gmail.com>
2022-04-26 09:09:46 +0000

    initial changes

---
+++ beerlog/models.py
@@ -0,0 +1,22 @@
+#from dataclasses import dataclass ## já acompanha o python, não precisa instalar
+from typing import Optional
+from sqlmodel import SQLModel
+from sqlmodel import select
+
+#@dataclass
+# class Beer():
+#     name: str
+#     style: str
+#     flavor: int
+#     image: int
+#     cost: int
+
+class Beer(SQLModel, table=True): #herança. Com isso ganhamos um ORM
+    id: Optional[int] = Field(primary_key=True, default=None, index=True)
+    name: str
+    style: str
+    flavor: int
+    image: int
+    cost: int
+
+brewdog = Beer(name="Brewdog", style="NEIPA", flavor=6, image=8, cost=8)
